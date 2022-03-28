d5ae7d3b2107e586377d469cf5cc1ce2c2d03164
suhanibansal870 <83600598+suhanibansal870@users.noreply.github.com>
2022-03-28 10:32:37 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDyiBj9Riw7qVQDlY-iksi-PWOYyyDEzq0",
+    authDomain: "let-s-chat-3aee4.firebaseapp.com",
+    projectId: "let-s-chat-3aee4",
+    storageBucket: "let-s-chat-3aee4.appspot.com",
+    messagingSenderId: "385457587242",
+    appId: "1:385457587242:web:bb3f28a81db344b6232f53"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
